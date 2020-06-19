33f803c51d5f4fc17d876b2b04d3752958e1d67b
gungun5940 <surinthorngun@gmail.com>
2020-06-19 14:50:38 +0700

    update showpdf

--- admin/result/showfile.php
+++ admin/result/showfile.php
@@ -7,6 +7,7 @@ $sql = new SQLiManager();
 if (!empty($_GET["id"])) {
     $sql->table = "file";
     $sql->field = "*";
+    $sql->condition = "WHERE ts_id = {$_GET['id']}";
     $query = $sql->select();
     while ($row = $query->fetch_object()) {
         $pdf = $row->ts_file;
@@ -16,7 +17,7 @@ if (!empty($_GET["id"])) {
     if (!file_exists($file)) {
         die("NO FILE HERE");
     }
-    
+
     // Add header to load pdf file
     header('Content-type: application/pdf');
     // header('Content-Disposition: attachment; filename="' . $file . '"'); //download file
